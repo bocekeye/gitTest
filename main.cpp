9fab90906f1b4e9ddf11d8b046c972809c17c1f9
fko2247103 <fko2247103@FIC-PCROOM3B-40>
2022-09-27 12:08:43 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include "DxLib.h"
+
+#include "game.h"
+
+// プログラムは WinMain から始まります
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	//ウィンドウモード設定
+	ChangeWindowMode(Game::kWindowMode);
+	//ウィンドウモード名設定
+	SetMainWindowText(Game::kTitkeText);
+	//画面のサイズ
+	SetGraphMode(Game::kScreenWidth, Game::kScreenHeight, Game::kColorDepht);
+
+	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
+	{
+		return -1;			    // エラーが起きたら直ちに終了
+	}
+	
+	//ダブルバッファモード
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	while (ProcessMessage() == 0)
+	{
+		LONGLONG time = GetNowHiPerformanceCount();
+
+		//画面のクリア
+		ClearDrawScreen();
+
+
+		DrawString(50, 50, "gitのテストやってます", GetColor(55, 123, 255));
+
+		//裏画面を表画面を入れ替える
+		ScreenFlip();
+
+		//escキーを押したら終了
+		if (CheckHitKey(KEY_INPUT_ESCAPE)) break;
+
+		while (GetNowHiPerformanceCount() - time < 1667)
+		{
+
+		}
+
+	}
+	
+	DxLib_End();				// ＤＸライブラリ使用の終了処理
+
+	return 0;				    // ソフトの終了 
+}
